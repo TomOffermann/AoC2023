0163919b1f3fd58452b3f47481698fbe1dd6d8c8
Tom Offermann <tom@offermann.name>
2023-12-19 22:55:38 +0100

    Day 17 & 19

--- Day17/day17.cpp
+++ Day17/day17.cpp
@@ -2,25 +2,118 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
-#include <unordered_set>
+#include <set>
 #include <unordered_map>
 #include <numeric>
 #include <stack>
+#include <queue>
+
+bool in_bounds(int x, int y, int W, int H) {
+  return x >= 0 && x < W && y >= 0 && y < H;
+}
+
+enum DIR {LEFT = 0, RIGHT = 1, UP = 2, DOWN = 3};
+
+struct edge {
+  int weight;
+  int vertex;
+  DIR dir;
+};
+
+int dijkstra(int W, int H, std::vector<std::string>& map, int min, int max) {
+  std::priority_queue<std::tuple<int,int,int,int>, std::vector<std::tuple<int,int,int,int>>, std::greater<std::tuple<int,int,int,int>>> pq;
+  
+  bool visited[W * H][4][max+2];
+  for(int i = 0; i < W * H; i++) {
+    for(int j = 0; j < 4; j++) {
+      for(int k = 0; k <= max + 1; k++) {
+        visited[i][j][k] = false;
+      }
+    }
+  }
+ 
+  std::vector<int> dist(W * H, 999999);
+ 
+  pq.push({0, 0, RIGHT, 1}); // cost, vertex, direction, on_straight
+  pq.push({0, 0, DOWN, 1});  // cost, vertex, direction, on_straight
+
+  while (!pq.empty()) {
+      int cost_u = std::get<0>(pq.top());
+      int u = std::get<1>(pq.top());
+      DIR dir_u = (DIR) std::get<2>(pq.top());
+      int on_straight_u = std::get<3>(pq.top());
+      pq.pop();
+
+      dist[u] = std::min(dist[u], cost_u);
+
+      if(visited[u][dir_u][on_straight_u]){
+        continue;
+      } else {
+        visited[u][dir_u][on_straight_u] = true;
+      }
+
+      int x = u % W;
+      int y = u / W;
+
+      int new_x = dir_u == LEFT ? x-1 : (dir_u == RIGHT ? x+1 : x);
+      int new_y = dir_u == UP ? y-1 : (dir_u == DOWN ? y+1 : y);
+
+      if(!in_bounds(new_x,new_y,W,H)) continue;
+
+      int new_cost = cost_u + (map[new_y][new_x] - '0');
+
+      std::vector<DIR> S;
+      if(on_straight_u >= min && on_straight_u <= max) {
+        if(new_x == W-1 && new_y == H-1) return new_cost;
+      }
+      if(dir_u == LEFT) {
+        S.push_back(LEFT);
+        S.push_back(UP);
+        S.push_back(DOWN);
+      } else if(dir_u == DOWN) {
+        S.push_back(LEFT);
+        S.push_back(RIGHT);
+        S.push_back(DOWN);
+      } else if(dir_u == RIGHT) {
+        S.push_back(RIGHT);
+        S.push_back(UP);
+        S.push_back(DOWN);
+      } else {
+        S.push_back(LEFT);
+        S.push_back(RIGHT);
+        S.push_back(UP);
+      }
+      for(int i = 0; i < S.size(); i++) {
+        int new_on_straight = dir_u == S[i] ? on_straight_u + 1 : 1;
+        if((dir_u != S[i] && on_straight_u < min) || new_on_straight > max) continue;
+        pq.push(std::make_tuple(new_cost, new_y * W + new_x, S[i], new_on_straight));
+      }
+  }
+  return -1;
+}
 
 int main() {
   std::fstream file;
-  file.open("./test.txt", std::ios::in); 
+  file.open("./input.txt", std::ios::in); 
 
   int res_1 = 0;
   int res_2 = 0;
 
+  std::vector<std::string> map;
+
   if(file.is_open()) {
     std::string str;
     while(getline(file, str)) {
-      
+      map.push_back(str);
     }
     file.close();
   };
+
+  int W = map[0].length() ;
+  int H = map.size();
+
+  res_1 = dijkstra(W, H, map, 1, 3);
+  res_2 = dijkstra(W, H, map, 4, 10);
 
   std::cout << "Task 1: " << res_1 << ", Task 2: " << res_2 << std::endl;
 }
--- Day19/day19.cpp
+++ Day19/day19.cpp
@@ -2,25 +2,265 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
-#include <unordered_set>
-#include <unordered_map>
+#include <set>
+#include <map>
 #include <numeric>
 #include <stack>
+#include <functional>
+
+struct RULE {
+  std::function<bool(struct NODE)> validate;
+  std::string vertex;
+  char type;
+  bool greater;
+  int val;
+};
+
+struct NODE {
+  int x;
+  int m;
+  int a;
+  int s;
+};
+
+struct INTERVAL {
+  long long int start;
+  long long int end;
+};
+
+bool valid(struct INTERVAL A) {
+  return A.start < A.end;
+}
+
+struct INTERVAL merge(struct INTERVAL A, struct INTERVAL B) {
+  struct INTERVAL C = {std::max(A.start, B.start), std::min(A.end, B.end)};
+  if((A.end > B.start && A.start < B.end) || (A.start < B.end && A.end > B.start)) return C;
+  return {0,0};
+}
+
+struct NODE_INT {
+  struct INTERVAL x;
+  struct INTERVAL m;
+  struct INTERVAL a;
+  struct INTERVAL s;
+};
+
+long long int DEEP_DFS(std::map<std::string, std::vector<struct RULE>>& G) {
+  std::vector<struct NODE_INT> intervals;
+
+  // DFS:
+  std::stack<std::pair<std::string, struct NODE_INT>> S;
+  S.push({"in", {{1,4000},{1,4000},{1,4000},{1,4000}}});
+  while(!S.empty()) {
+    std::pair<std::string, struct NODE_INT> curr_s = S.top();
+    std::string vertex = curr_s.first;
+    struct NODE_INT interval = curr_s.second;
+    S.pop();
+    for(int i = 0; i < G[vertex].size(); i++) {
+      int v = G[vertex][i].val;
+      struct INTERVAL g = {v+1,4000};
+      struct INTERVAL ge = {v,4000};
+      struct INTERVAL l = {1, v-1};
+      struct INTERVAL le = {1, v};
+      struct NODE_INT copy = interval;
+      switch (G[vertex][i].type) {
+        case 'x':
+          copy.x = G[vertex][i].greater ? merge(g, interval.x) : merge(l, interval.x);
+          interval.x = G[vertex][i].greater ? merge(le, interval.x) : merge(ge, interval.x);
+          if(valid(copy.x)) {
+            if(G[vertex][i].vertex == "A") {
+              intervals.push_back(copy);
+            } else {
+              S.push({G[vertex][i].vertex, copy});
+            }
+          }
+          break;
+        case 'm':
+          copy.m = G[vertex][i].greater ? merge(g, interval.m) : merge(l, interval.m);
+          interval.m = G[vertex][i].greater ? merge(le, interval.m) : merge(ge, interval.m);
+          if(valid(copy.m)) {
+            if(G[vertex][i].vertex == "A") {
+              intervals.push_back(copy);
+            } else {
+              S.push({G[vertex][i].vertex, copy});
+            }
+          }
+          break;
+        case 'a':
+          copy.a = G[vertex][i].greater ? merge(g, interval.a) : merge(l, interval.a);
+          interval.a = G[vertex][i].greater ? merge(le, interval.a) : merge(ge, interval.a);
+          if(valid(copy.a)) {
+            if(G[vertex][i].vertex == "A") {
+              intervals.push_back(copy);
+            } else {
+              S.push({G[vertex][i].vertex, copy});
+            }
+          }
+          break;
+        case 's':
+          copy.s = G[vertex][i].greater ? merge(g, interval.s) : merge(l, interval.s);
+          interval.s = G[vertex][i].greater ? merge(le, interval.s) : merge(ge, interval.s);
+          if(valid(copy.s)) {
+            if(G[vertex][i].vertex == "A") {
+              intervals.push_back(copy);
+            } else {
+              S.push({G[vertex][i].vertex, copy});
+            }
+          }
+          break;
+        case '*':
+          if(G[vertex][i].vertex == "A") {
+            intervals.push_back(interval);
+          } else {
+            if(G[vertex][i].vertex != "R") S.push({G[vertex][i].vertex, interval});
+          }
+          break;
+      }
+    }
+  }
+
+  // Calculation:
+  long long res = 0;
+  for(int i = 0; i < intervals.size(); i++) {
+    res += (intervals[i].x.end - intervals[i].x.start + 1) * 
+           (intervals[i].m.end - intervals[i].m.start + 1) * 
+           (intervals[i].a.end - intervals[i].a.start + 1) * 
+           (intervals[i].s.end - intervals[i].s.start + 1);
+  }
+  return res;
+}
+
+bool DFS(std::map<std::string, std::vector<struct RULE>>& G, struct NODE node) {
+  std::map<std::string, bool> visited;
+
+  for(const auto &p : G) {
+    visited[p.first] = false;
+  }
+
+  std::stack<std::string> S;
+  S.push("in");
+  visited["in"] = true;
+  while(!S.empty()) {
+    std::string curr = S.top();
+    S.pop();
+    for(int i = 0; i < G[curr].size(); i++) {
+      if(!visited[G[curr][i].vertex] && G[curr][i].validate(node)) {
+        if(G[curr][i].vertex == "A") return true;
+        if(G[curr][i].vertex == "R") return false;
+        S.push(G[curr][i].vertex);
+        visited[G[curr][i].vertex] = true;
+        break;
+      }
+    }
+  }
+  return false;
+}
 
 int main() {
   std::fstream file;
-  file.open("./test.txt", std::ios::in); 
+  file.open("./input.txt", std::ios::in); 
 
   int res_1 = 0;
-  int res_2 = 0;
+  long long int res_2 = 0;
+
+  std::map<std::string, std::vector<struct RULE>> G;
+  std::vector<struct NODE> V;
 
   if(file.is_open()) {
     std::string str;
+    bool parse_G = true;
     while(getline(file, str)) {
-      
+      int n = str.length();
+      if(n == 0) {
+        parse_G = false;
+        continue;
+      }
+      if (parse_G) {
+        std::string start = "";
+        std::vector<struct RULE> rules;
+        int i = 0;
+        for(; i < n && str[i] != '{'; i++) {
+          start += str[i];
+        }
+        i++;
+        while(i < n) {
+          std::string category = "";
+          for(; i < n && str[i] != '}' && str[i] != '<' && str[i] != '>'; i++) {
+            category += str[i];
+          }
+          char op = str[i];
+          i++;
+          if(op == '}') {
+            rules.push_back({[](struct NODE node){return true;}, category, '*', false, 0});
+            break;
+          } else {
+            std::string num = "";
+            std::string vertex = "";
+            for(; i < n && isdigit(str[i]); i++) {
+              num += str[i];
+            }
+            i++;
+            for(; i < n && str[i] != ',' && str[i] != '}'; i++) {
+              vertex += str[i];
+            }
+
+            std::function<bool(struct NODE)> validate = [category, num, op](struct NODE node){
+              bool greater = op == '>';
+              int val = std::stoi(num);
+              if(category == "x") {
+                return greater ? node.x > val : node.x < val;
+              } else if(category == "m") {
+                return greater ? node.m > val : node.m < val;
+              } else if(category == "a") {
+                return greater ? node.a > val : node.a < val;
+              } else {
+                return greater ? node.s > val : node.s < val;
+              } 
+            };
+
+            struct RULE r = {validate, vertex, category[0], op == '>', std::stoi(num)};
+            rules.push_back(r);
+          }
+          i++;
+        }
+        G[start] = rules;
+      } else {
+        int i = 0;
+        for(; i < n && !isdigit(str[i]); i++);
+        std::string x;
+        for(; i < n && isdigit(str[i]); i++) {
+          x += str[i];
+        }
+        for(; i < n && !isdigit(str[i]); i++);
+        std::string m;
+        for(; i < n && isdigit(str[i]); i++) {
+          m += str[i];
+        }
+        for(; i < n && !isdigit(str[i]); i++);
+        std::string a;
+        for(; i < n && isdigit(str[i]); i++) {
+          a += str[i];
+        }
+        for(; i < n && !isdigit(str[i]); i++);
+        std::string s;
+        for(; i < n && isdigit(str[i]); i++) {
+          s += str[i];
+        }
+  
+        struct NODE node = {std::stoi(x), std::stoi(m), std::stoi(a), std::stoi(s)};
+        V.push_back(node);
+      }
     }
     file.close();
   };
   
+  for(int i = 0; i < V.size(); i++) {
+    if(DFS(G, V[i])) {
+      res_1 += (V[i].x + V[i].m + V[i].a + V[i].s);
+    }
+  }
+
+  res_2 = DEEP_DFS(G);
+
   std::cout << "Task 1: " << res_1 << ", Task 2: " << res_2 << std::endl;
 }
